a91ab9b6a5d37276b2201b4f6913c97f84698606
Ayush Raj <rayushkgp@gmail.com>
2023-07-27 17:39:24 +0530

    first commit

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from urllib.request import urlopen as uReq
 import logging
 logging.basicConfig(filename="scrapper.log" , level=logging.INFO)
 
+import pymongo
 app = Flask(__name__)
 
 @app.route("/", methods = ['GET'])
@@ -72,6 +73,11 @@ def index():
                           "Comment": custComment}
                 reviews.append(mydict)
             logging.info("log my final result {}".format(reviews))
+
+            client = pymongo.mongoClient("mongodb+srv://ayushsbp:AyushBP2@cluster0.ca609ii.mongodb.net/?retryWrites=true&w=majority")
+            db = client['review_scrap']
+            review_col = db['review_scrap_data']
+            review_col.insert_many(reviews)
             return render_template('result.html', reviews=reviews[0:(len(reviews)-1)])
         except Exception as e:
             logging.info(e)
